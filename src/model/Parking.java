23650d9565f2dceb28256c545f80c1c8c9506dcd
nellyiriberri <nellyiriberri@gmail.com>
2020-05-26 09:15:58 +0200

    Add toll gate inner class in parking class

--- src/model/Parking.java
+++ src/model/Parking.java
@@ -9,10 +9,58 @@ public class Parking<T> {
     public String name;
     public int capacity = 15;
     public List<T> vehicles = new ArrayList<>();
+    public List<TollGate> tollGates = new ArrayList<>();
+
+    /**
+     * Toll gate inner class
+     */
+    private class TollGate{
+
+        public String id;
+        public boolean enable = true;
+        public Direction direction;
+        public Orientation orientation;
+
+        public TollGate(){
+
+        }
+
+    }
+
+    /**
+     * Enums inner classes
+     */
+    private enum Direction{
+        IN,
+        OUT,
+        INOUT
+    }
+
+    private enum Orientation{
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
 
     public Parking(String code, String name){
         this.code = code;
         this.name = name;
+        TollGate tg1 = new TollGate();
+        this.tollGates.add(tg1);
+        tg1.direction = Direction.IN;
+        tg1.orientation = Orientation.EAST;
+        tg1.id = code + "#" + "door-" + this.tollGates.size();
+        TollGate tg2 = new TollGate();
+        this.tollGates.add(tg2);
+        tg2.direction = Direction.OUT;
+        tg2.orientation = Orientation.SOUTH;
+        tg2.id = code + "#" + "door-" + this.tollGates.size();
+
+        System.out.println(name + " gates");
+        for(TollGate tg: tollGates) {
+            System.out.println("--Gate: " + tg.id);
+        }
     }
 
     public String calculateTotalPrice(){
